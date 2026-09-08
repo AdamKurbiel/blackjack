79b0e6b0cb4e6133fd1859bf020376a9d766d7bc
Adam Kurbiel <ad.kurbiel@gmail.com>
2026-09-08 19:23:41 +0200

    added shuffle function

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const KOLORY = ['kier', 'karo', 'trefl', 'pik'];
 const FIGURY = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+const KOMUNIKAT = document.getElementById("komunikat");
 
+function tasuj(talia){
+    let i = talia.length;
+
+    while (i != 0) {
+        let losowy_i = Math.floor(Math.random() * i);
+        i--;
+        [talia[i], talia[losowy_i]] = [
+        talia[losowy_i], talia[i]];
+  }
+}
 
 function stworzTalie(liczbaTalii){
     var talia = [];
@@ -9,12 +20,12 @@ function stworzTalie(liczbaTalii){
         for (kolor of KOLORY){
             for (figura of FIGURY){
                 talia.push({kolor, figura});
-            }
-        }
-    }
+    }}}
 
+    tasuj(talia);
     return talia;
 }
 
-
-console.log(stworzTalie(1));
+var talia = stworzTalie(1);
+var gracz = [];
+var krupier = [];
